cb85e6cbfee28f9e3b75db55d78d482d96d22ffe
[hanlei211] <[463779213@qq.com]>
2018-06-19 23:03:19 +0800

    热部署

--- src/main/java/com/hlc/sell/config/DruidConfig.java
+++ src/main/java/com/hlc/sell/config/DruidConfig.java
@@ -3,14 +3,18 @@ package com.hlc.sell.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
--- src/main/java/com/hlc/sell/controller/HelloController.java
+++ src/main/java/com/hlc/sell/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.hlc.sell.controller;
 
-import org.springframework.stereotype.Controller;
+import com.hlc.sell.config.DruidConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/index")
    private  String  index(){
         return "index";
     }
+
+    @RequestMapping("/indexindex")
+    private  String  index1(){
+        return "index";
+    }
+
+
 }
